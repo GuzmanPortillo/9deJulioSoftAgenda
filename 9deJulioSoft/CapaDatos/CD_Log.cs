ba786ad98bf7a6d92b522ab0b7e2716a5f90270e
pedroi77 <iralapedro@gmail.com>
2022-03-12 17:28:50 -0300

    Create CD_Log.cs

---
+++ 9deJulioSoft/CapaDatos/CD_Log.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CapaDatos
+{
+    public class CD_Log : ConnectionToSql
+    {
+        public void Insertar(int usuario, string accion, string detalle)
+        {
+            string sSql = "Insert Into Bitacora" +
+            "(fecha, id_usuario, accion, detalle)" +
+            " Values( " + "getdate(), " + usuario + ","  + "'" + accion + "'," + "'" + detalle + "')";
+
+            using (var connection = GetConnection())
+            {
+
+                connection.Open();
+
+                SqlCommand cmd = new SqlCommand(sSql, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            //objDB.Ejecutar(sSql);
+        }
+
+    }
+}
