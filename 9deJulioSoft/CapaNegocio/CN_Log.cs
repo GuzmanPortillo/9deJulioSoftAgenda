ef7e7482644bcdf9b3ce2dddbc93d2134bd2ace7
pedroi77 <iralapedro@gmail.com>
2022-03-12 17:30:35 -0300

    Create CN_Log.cs

---
+++ 9deJulioSoft/CapaNegocio/CN_Log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using CapaDatos;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaNegocio
+{
+    public static class CN_Log
+    {
+        //public int id { get; set; }
+        //public DateTime fecha { get; set; }
+        //public int usuario { get; set; }
+        //public string accion { get; set; }
+        //public string detalle { get; set; }
+
+        public static void Insertar(int usuario,  string accion, string detalle)
+        {
+            var logDatos = new CD_Log();
+            logDatos.Insertar(usuario, accion, detalle);
+        }
+
+
+
+
+    }
+}
